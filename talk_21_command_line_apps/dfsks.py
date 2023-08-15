ba317416aebd850c543b72bb769504a344528c22
Henrik Andersson <jan@dhigroup.com>
2023-08-15 21:29:39 +0200

    Getting there

--- talk_21_command_line_apps/dfsks.py
+++ talk_21_command_line_apps/dfsks.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python
 from datetime import datetime
-from typing import Optional
+from enum import Enum
+from typing import Annotated, Optional, List
+from pathlib import Path
+import json
 import typer
 import mikeio
 import mikeio.generic as mg
 
+
 app = typer.Typer(add_completion=False)
 
 
+class OutputFormat(str, Enum):
+    raw = "raw"
+    json = "json"
+
+
 @app.command()
-def list(filename: str):
-    dfs = mikeio.open(filename)
+def list(path: Path, format: OutputFormat = OutputFormat.raw):
+
+    if not path.exists():
+        typer.echo(f"File does not exist: {path}")
+        raise typer.Exit(code=1)
+
+    dfs = mikeio.open(path)
     start_time = dfs.time[0]
     end_time = dfs.time[-1]
-    print(f"File: {filename} ({start_time} - {end_time})")
+    print(f"File: {path} ({start_time} - {end_time})")
 
-    for item in dfs.items:
-        print(f"* {item.name} ({item.unit.name})")
+    if format == OutputFormat.raw:
+        for item in dfs.items:
+            print(f"* {item.name} ({item.unit.name})")
+    elif format == OutputFormat.json:
+        res = []
+        for item in dfs.items:
+            res.append({"name": item.name, "unit": item.unit.name})
+        print(json.dumps(res, indent=2))
 
 
 @app.command()
 def extract(
     infilename: str,
     outfilename: str,
-    item: Optional[str] = None,
+    item: Optional[List[str]] = typer.Option(None,"---item", "-i",help="Item name, default is all items") ,
     start: Optional[datetime] = None,
     end: Optional[datetime] = None,
 ):
-    """Extract a subset of a dfs file
-    
-    The default is to extract all items and all time steps.
-    """
+    """Extract a subset of a dfs file"""
     mg.extract(
         infilename=infilename,
         outfilename=outfilename,
