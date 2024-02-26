c0ad7c84bc9100e5f7890b2849a75ff04f20bf2c
Henrik Andersson <jan@dhigroup.com>
2024-02-26 10:55:22 +0100

    Draft content

--- talk_29_polars/scripts/get_data.py
+++ talk_29_polars/scripts/get_data.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-from nycflights13 import flights
+from nycflights13 import flights, airports, airlines, weather
 
 # create data directory
 Path("data").mkdir(exist_ok=True)
 
 flights.to_csv("data/flights.csv.zip", index=False, compression="gzip")
+airports.to_csv("data/airports.csv", index=False)
